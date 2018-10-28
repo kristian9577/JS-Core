0516c2c90030477633251cabc8d42c3c0e156283
kristian9577 <kristian9577@gmail.com>
2018-10-28 11:51:02 +0200

    add files

---
+++ JS-Advanced/04.jQuery Library - Lab/02.search.js
@@ -0,0 +1,9 @@
+function search() {
+    let searchText = $('#searchText').val();
+    let matchedElements =
+        $(`#towns li:contains('${searchText}')`);
+    $("#towns li").css('font-weight', '');
+    matchedElements.css('font-weight', 'bold');
+    $("#result").text(matchedElements.length
+        + ' maches found.');
+}
